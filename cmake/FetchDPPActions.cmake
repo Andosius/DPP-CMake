01bf73b2f1a72b99d61f1d5394372b7e1f702d81
Denis Ahmatovic <37641046+Andosius@users.noreply.github.com>
2023-07-20 11:40:13 +0200

    cd into build dir to fix cmake --build error

--- cmake/FetchDPPActions.cmake
+++ cmake/FetchDPPActions.cmake
@@ -11,7 +11,7 @@ function(DPP_BuildFromSourceUnix)
 	execute_process(COMMAND "cmake" "-B" "${DPP_BUILD_HOME}/build" "-S" "${DPP_BUILD_HOME}" "-DDPP_NO_VCPKG=ON" "-DCMAKE_BUILD_TYPE=Release")
 	
 	# Build all the files
-	execute_process(COMMAND "cmake" "--build" "${DPP_BUILD_HOME}/build" "-j")
+	execute_process(COMMAND "cd" "${DPP_BUILD_HOME}" "&&" "cmake" "--build" "./build" "-j")
 	
 	# Install libdpp :)
 	execute_process(COMMAND "make" "-C" "${DPP_BUILD_HOME}/build" "install")
